7c2193a072c5f941fcdedcc12fb4e3732062576e
Carlos Souza <carloshrsouza@gmail.com>
2014-09-24 11:24:38 -0400

    Fix port for Heroku.

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'));
 var blocks = require('./routes/blocks');
 app.use('/blocks', blocks);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Running Express!');
 });
