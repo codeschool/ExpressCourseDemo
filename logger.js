5270bda37fafac331304abbbfe887d65a6f2417a
Carlos Souza <carloshrsouza@gmail.com>
2014-10-16 08:38:39 -0400

    Simplify logger example

--- logger.js
+++ logger.js
@@ -1,15 +1,14 @@
 module.exports = function(request, response, next) {
-  var start = process.hrtime();
+  var start = +new Date();
   var stream = process.stdout;
   var url = request.url;
   var method = request.method;
 
   response.on('finish', function() {
-    var diff = process.hrtime(start);
-    var duration = diff[0] * 1*1000 + diff[1] * 1/1000000
+    var duration = +new Date() - start;
 
     var message = method  + ' to ' + url +
-      '\ntook ' + duration.toFixed(3) + ' milliseconds \n\n';
+      '\ntook ' + duration + ' milliseconds \n\n';
 
     stream.write(message);
   });
